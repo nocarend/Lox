a3805556c543926ffdf7d7edd674ed8a33d29423
Nikolay <58514394+nocarend@users.noreply.github.com>
2025-07-19 15:47:55 +0700

    4.5: recognising lexems

--- src/main/scala/com/example/lox/Scanner.scala
+++ src/main/scala/com/example/lox/Scanner.scala
@@ -1,25 +1,58 @@
 package com.example.lox
 
-import com.example.lox.TokenType.EOF
+import com.example.lox.TokenType.*
 
 import scala.collection.mutable
 import scala.collection.mutable.ArrayBuffer
 
 class Scanner(val source: String) {
   private val tokens: mutable.ArrayBuffer[Token] = ArrayBuffer()
-  private var start:   Int = 0
+  private var start: Int = 0
   private var current: Int = 0
-  private var line:    Int = 1
-  
+  private var line: Int = 1
+
   def scanTokens(): ArrayBuffer[Token] = {
     while (!isAtEnd()) {
       start = current
       scanToken()
     }
-    
+
     tokens.addOne(Token(EOF, "", null, line))
   }
-  
+
+  private def scanToken(): Unit = {
+    val c: Char = advance()
+    val tokenType: TokenType = c match {
+      case '(' => LEFT_PAREN
+      case ')' => RIGHT_PAREN
+      case '{' => LEFT_BRACE
+      case '}' => RIGHT_BRACE
+      case ',' => COMMA
+      case '.' => DOT
+      case '-' => MINUS
+      case '+' => PLUS
+      case ';' => SEMICOLON
+      case '*' => STAR
+    }
+
+    addToken(tokenType)
+  }
+
+  private def advance(): Char = {
+    val char = source.charAt(current)
+    current += 1
+    char
+  }
+
+  private def addToken(tokenType: TokenType): Unit = {
+    addToken(tokenType, null)
+  }
+
+  private def addToken(tokenType: TokenType, literal: Object): Unit = {
+    val text: String = source.substring(start, current)
+    tokens.addOne(Token(tokenType, text, literal, line))
+  }
+
   private def isAtEnd(): Boolean = {
     current >= source.length
   }
